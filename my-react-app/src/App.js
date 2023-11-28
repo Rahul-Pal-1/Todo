985e321acda56ad13ce6fe5fb9b8ab4d7f8a6a7d
Rahul Kumar Pal <01.rahulpal.0@gmail.com>
2023-11-28 21:21:21 +0530

    first commit

---
+++ my-react-app/src/App.js
@@ -0,0 +1,31 @@
+import React ,{useState} from 'react';
+import './App.css';
+import Header from './components/Header';
+import Form from './components/Form';
+import Todolist from './components/todolist';
+const App = () => {
+  const [input, setInput] = useState("");
+  const [todo, setTodo] = useState([]);
+  const [editTodo, setEditTodo] = useState(null);
+  return (
+    <div className='container'>
+      <div className='app-wrapper'> 
+        <div><Header/></div>
+        <div>
+          <Form  input={input}
+                setInput = {setInput}
+                todo = {todo}
+                setTodo = {setTodo}
+                setEditTodo={setEditTodo}
+                editTodo = {editTodo}
+          />
+        </div>
+        <div>
+          <Todolist todo={todo} setTodo={setTodo} setEditTodo={setEditTodo}/>
+        </div>
+      </div> 
+    </div>
+  );
+};
+
+export default App;
---
+++ my-react-app/src/components/todolist.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const Todolist = ({ todo, setTodo, setEditTodo }) => {
+    const change = (event) => {
+        event.preventDefault();
+    };
+
+    const handleComplete = (selectedTodo) => {
+        setTodo(todo.map((item) => {
+            if (item.id === selectedTodo.id) {
+                return { ...item, completed: !item.completed };
+            }
+            return item;
+        }));
+    };
+
+    const handleEdit = ({ id }) => {
+        const edit = todo.find((todos) => todos.id === id);
+        setEditTodo(edit);
+        
+    };
+
+    const handleDelete = ({ id }) => {
+        setTodo(todo.filter((todos) => todos.id !== id));
+    };
+
+    return (
+        <div>
+            {todo.map((todos) => (
+                <li className={`${todos.completed ? 'complete list-item' : "list-item"}`} key={todos.id}>
+                    <input type="text" value={todos.title} className="list" onChange={change} />
+                    <div>
+                        <button className="button-complete task-button" onClick={() => handleComplete(todos)}>
+                            <i className="fa fa-check-circle"></i>
+                        </button>
+                        <button className="button-edit task-button" onClick={() => handleEdit(todos)}>
+                            <i className="fa fa-edit"></i>
+                        </button>
+                        <button className="button-delete task-button" onClick={() => handleDelete(todos)}>
+                            <i className="fa fa-trash"></i>
+                        </button>
+                    </div>
+                </li>
+            ))}
+        </div>
+    );
+};
+
+export default Todolist;
